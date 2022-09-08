7a0c7adf1c48824a4f7569e368a2ec29318ec994
Muluken Hakim <91600753+MuleHakim@users.noreply.github.com>
2022-09-08 17:19:43 +0300

    main.c added

--- main.c
+++ main.c
@@ -1,47 +1,48 @@
 #include "monty.h"
-
-/* global struct to hold flag for queue and stack length */
-var_t var;
-
+globals_t global;
 /**
- * main - Monty bytecode interpreter
- * @argc: number of arguments passed
- * @argv: array of argument strings
- *
- * Return: EXIT_SUCCESS on success or EXIT_FAILURE on failure
+ * main - main function
+ * @argc: number of the arguments
+ * @argv: argument
+ * Return: Always 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	stack_t *stack = NULL;
-	unsigned int line_number = 0;
-	FILE *fs = NULL;
-	char *lineptr = NULL, *op = NULL;
-	size_t n = 0;
+	size_t numbytes = 0;
+	int bytesr = 0;
+	unsigned int con = 1;
 
-	var.queue = 0;
-	var.stack_len = 0;
+	global.flag = 1;
+	global.line = NULL;
 	if (argc != 2)
 	{
-		dprintf(STDOUT_FILENO, "USAGE: monty file\n");
+		fputs("USAGE: monty file\n", stderr);
 		exit(EXIT_FAILURE);
 	}
-	fs = fopen(argv[1], "r");
-	if (fs == NULL)
+	global.fil = fopen(argv[1], "r");
+	if (global.fil == NULL)
 	{
-		dprintf(STDOUT_FILENO, "Error: Can't open file %s\n", argv[1]);
+		dprintf (2, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	on_exit(free_lineptr, &lineptr);
-	on_exit(free_stack, &stack);
-	on_exit(m_fs_close, fs);
-	while (getline(&lineptr, &n, fs) != -1)
+	while ((bytesr = getline(&global.line, &numbytes, global.fil)) != EOF)
 	{
-		line_number++;
-		op = strtok(lineptr, "\n\t\r ");
-		if (op != NULL && op[0] != '#')
+		delete_jump(global.line);
+		if (global.line[0] != 35)
 		{
-			get_op(op, &stack, line_number);
+			global.token = strtok(global.line, " \t\n");
+			global.opco = global.token;
+			if (global.opco != NULL)
+			{
+				global.token = strtok(NULL, " \t\n");
+				func(global.opco)(&stack, con);
+			}
+			con++;
 		}
 	}
-	exit(EXIT_SUCCESS);
+	free_l(&stack);
+	free(global.line);
+	fclose(global.fil);
+	return (0);
 }
