601d413c9b148fb831b7ba7dbdfc19d6db4f34d1
Muluken Hakim <91600753+MuleHakim@users.noreply.github.com>
2022-09-08 17:36:25 +0300

    Task 13 rotl advanced

--- rotl.c
+++ rotl.c
@@ -1,16 +1,28 @@
 #include "monty.h"
-
 /**
- * rotl - rotates the stack to the top.
- * @stack:double pointer tot he begining of the linked list
- * @line_number: script line number
- *
- * Return: void
+ * _rotl - rotates the stack to the top.
+ * @stack: head
+ * @num_line: num line
  */
-void rotl(stack_t **stack, unsigned int line_number)
-{
-	(void)line_number;
 
-	if (*stack)
-		*stack = (*stack)->next;
+void _rotl(stack_t **stack, unsigned int num_line)
+{
+	stack_t *temp1;
+	stack_t *temp2;
+
+	if (*stack && (*stack)->next)
+	{
+		temp1 = *stack;
+		temp2 = temp1;
+		*stack = temp1->next;
+		(*stack)->prev = NULL;
+		if (num_line)
+			while (temp2->next)
+			{
+				temp2 = temp2->next;
+			}
+		temp2->next = temp1;
+		temp1->prev = temp2;
+		temp1->next = NULL;
+	}
 }
