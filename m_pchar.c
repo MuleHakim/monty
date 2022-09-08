6f6067fcea862743262452a6f7ef97167bf09c87
Muluken Hakim <91600753+MuleHakim@users.noreply.github.com>
2022-09-08 09:50:58 +0300

    Task 11 pchar advanced

--- m_pchar.c
+++ m_pchar.c
@@ -14,7 +14,7 @@ void m_pchar(stack_t **stack, unsigned int line_number)
 
 	if (var.stack_len < 1)
 	{
-		dprintf(STDOUT_FILENO,
+		vprintf(STDOUT_FILENO,
 			"L%u: can't pchar, stack empty\n",
 			line_number);
 		exit(EXIT_FAILURE);
